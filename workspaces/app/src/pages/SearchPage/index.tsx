8e561a024821a4921ca982c762ede6bedc00bbc6
Karibash <y.karibayashi@gmail.com>
2024-05-10 14:59:15 +0900

    feature: 本の検索絞り込み処理をバックエンド側で処理するようにする

--- workspaces/app/src/pages/SearchPage/index.tsx
+++ workspaces/app/src/pages/SearchPage/index.tsx
@@ -1,6 +1,6 @@
-import { Suspense, useCallback, useId, useState } from 'react';
+import { useCallback, useId, useState } from 'react';
+import { useDebounce } from 'react-use';
 
-import { useBookList } from '../../features/book/hooks/useBookList';
 import { Box } from '../../foundation/components/Box';
 import { Text } from '../../foundation/components/Text';
 import { Color, Space, Typography } from '../../foundation/styles/variables';
@@ -9,11 +9,10 @@ import { Input } from './internal/Input';
 import { SearchResult } from './internal/SearchResult';
 
 const SearchPage: React.FC = () => {
-  const { data: books } = useBookList({ query: {} });
-
   const searchResultsA11yId = useId();
 
   const [keyword, setKeyword] = useState('');
+  const [searchKeyword, setSearchKeyword] = useState('');
 
   const onChangedInput = useCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -22,6 +21,10 @@ const SearchPage: React.FC = () => {
     [setKeyword],
   );
 
+  useDebounce(() => {
+    setSearchKeyword(keyword);
+  }, 250, [keyword]);
+
   return (
     <Box px={Space * 2}>
       <Input onChange={onChangedInput} />
@@ -29,18 +32,10 @@ const SearchPage: React.FC = () => {
         <Text color={Color.MONO_100} id={searchResultsA11yId} typography={Typography.NORMAL20} weight="bold">
           検索結果
         </Text>
-        {keyword !== '' && <SearchResult books={books} keyword={keyword} />}
+        {searchKeyword !== '' && <SearchResult keyword={searchKeyword} />}
       </Box>
     </Box>
   );
 };
 
-const SearchPageWithSuspense: React.FC = () => {
-  return (
-    <Suspense fallback={null}>
-      <SearchPage />
-    </Suspense>
-  );
-};
-
-export { SearchPageWithSuspense as SearchPage };
+export { SearchPage };
