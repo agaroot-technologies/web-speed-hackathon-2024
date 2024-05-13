1becd406a7642249b501e0a8784f278f23cd6fbf
Karibash <y.karibayashi@gmail.com>
2024-05-13 14:46:12 +0900

    feature: 管理者画面の著者検索処理ををバックエンド側で処理するようにする

--- workspaces/admin/src/features/authors/hooks/useAuthorList.ts
+++ workspaces/admin/src/features/authors/hooks/useAuthorList.ts
@@ -1,12 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
 
+import type { GetAuthorListRequestQuery } from '@wsh-2024/schema/src/api/authors/GetAuthorListRequestQuery';
+
 import { authorApiClient } from '../apiClient/authorApiClient';
 
-export const useAuthorList = () => {
+
+export const useAuthorList = (query: GetAuthorListRequestQuery) => {
   return useQuery({
     queryFn: async ({ queryKey: [, options] }) => {
       return authorApiClient.fetchList(options);
     },
-    queryKey: authorApiClient.fetchList$$key({ query: {} }),
+    queryKey: authorApiClient.fetchList$$key({ query }),
   });
 };
