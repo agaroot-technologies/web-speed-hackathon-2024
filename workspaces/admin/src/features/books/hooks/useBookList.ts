5b9e950764e0a532739a11376675db08a75c2b93
Karibash <y.karibayashi@gmail.com>
2024-05-15 15:08:12 +0900

    feature: 管理者画面の書籍検索処理ををバックエンド側で処理するようにする

--- workspaces/admin/src/features/books/hooks/useBookList.ts
+++ workspaces/admin/src/features/books/hooks/useBookList.ts
@@ -1,12 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 
+import type { GetBookListRequestQuery } from '@wsh-2024/schema/src/api/books/GetBookListRequestQuery';
+
 import { bookApiClient } from '../apiClient/bookApiClient';
 
-export const useBookList = () => {
+export const useBookList = (query: GetBookListRequestQuery = {}) => {
   return useQuery({
     queryFn: async ({ queryKey: [, options] }) => {
       return bookApiClient.fetchList(options);
     },
-    queryKey: bookApiClient.fetchList$$key({ query: {} }),
+    queryKey: bookApiClient.fetchList$$key({ query }),
   });
 };
