19b6029d99b1bc7b362337b19536dfd550bc9cb4
Karibash <y.karibayashi@gmail.com>
2024-05-24 14:30:45 +0900

    feature: 本日更新リストの不要なレスポンスを削除する

--- workspaces/server/src/repositories/release.ts
+++ workspaces/server/src/repositories/release.ts
@@ -45,32 +45,20 @@ class ReleaseRepository implements ReleaseRepositoryInterface {
         with: {
           books: {
             columns: {
-              description: true,
               id: true,
               name: true,
             },
             with: {
               author: {
                 columns: {
-                  description: true,
-                  id: true,
                   name: true,
                 },
                 with: {
                   image: {
                     columns: {
-                      alt: true,
                       id: true,
                     },
                   },
-                },
-              },
-              episodes: {
-                columns: {
-                  chapter: true,
-                  description: true,
-                  id: true,
-                  name: true,
                 },
               },
               image: {
